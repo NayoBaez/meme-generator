3efe9764ea4dd4fc190b5c5dc4e39aa0cefd0fb6
NayoBaez <naomi.singh@hotmail.com>
2023-02-22 16:44:35 -0400

    Feature: add footer link to repo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Footer() {
-  return <>Hello from footer</>;
+  return (
+    <footer>
+      <a href="https://github.com/NayoBaez/meme-generator" target="blank">
+        Open-Source Code
+      </a>
+      <span> by Naomi</span>
+    </footer>
+  );
 }

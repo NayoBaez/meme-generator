f47625afad84d3590cf522e730c555f23006b489
NayoBaez <naomi.singh@hotmail.com>
2023-02-22 17:21:58 -0400

    Feature: add meme image and overlay text

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Meme() {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
   return (
     <main>
       <div className="form">
         <input type="text" placeholder="Top text" className="form--input" />
         <input type="text" placeholder="Bottom text" className="form--input" />
         <button className="form--button"> Get a new meme image 🖼 </button>
+      </div>
+      <div className="meme">
+        <img src="http://i.imgflip.com/1bij.jpg" className="meme--image" />
+        <h2 className="meme--text top">text here</h2>
+        <h2 className="meme--text bottom">text here</h2>
       </div>
     </main>
   );

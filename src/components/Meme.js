d85c74a66c3d6c9ec2c5d0f3f9eff2a295dea193
NayoBaez <naomi.singh@hotmail.com>
2023-02-22 17:35:46 -0400

    Feature: add state & handleChange to form input

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -6,17 +6,40 @@ export default function Meme() {
     bottomText: "",
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }
+
   return (
     <main>
       <div className="form">
-        <input type="text" placeholder="Top text" className="form--input" />
-        <input type="text" placeholder="Bottom text" className="form--input" />
+        <input
+          type="text"
+          placeholder="Top text"
+          className="form--input"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="form--input"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
         <button className="form--button"> Get a new meme image 🖼 </button>
       </div>
       <div className="meme">
-        <img src="http://i.imgflip.com/1bij.jpg" className="meme--image" />
-        <h2 className="meme--text top">text here</h2>
-        <h2 className="meme--text bottom">text here</h2>
+        <img src={meme.randomImage} className="meme--image" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
       </div>
     </main>
   );

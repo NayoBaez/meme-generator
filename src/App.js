ad0b14e0ccb183f2351310d94b1a0d5f94c115cd
NayoBaez <naomi.singh@hotmail.com>
2023-02-22 16:34:10 -0400

    Feature: add header title & image

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Meme from "./components/Meme";
 import Footer from "./components/Footer";
+import "./App.css";
 
 export default function App() {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Header() {
-  return <>Hello from Header</>;
+  return (
+    <header className="header">
+      <img src="../images/troll-face.png" className="header--image" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project"> Easily add text to images or memes</h4>
+    </header>
+  );
 }

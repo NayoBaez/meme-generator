74137b9aaa376891de7fb574f7c34a5d50ea44ba
NayoBaez <naomi.singh@hotmail.com>
2023-02-23 16:35:42 -0400

    Fix: alt error header component for netlify deployment

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,11 @@ import React from "react";
 export default function Header() {
   return (
     <header className="header">
-      <img src="../images/troll-face.png" className="header--image" />
+      <img
+        src="../images/troll-face.png"
+        alt="troll-face"
+        className="header--image"
+      />
       <h2 className="header--title">Meme Generator</h2>
       <h4 className="header--project"> Easily add text to images or memes</h4>
     </header>
